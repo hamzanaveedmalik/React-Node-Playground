6282516eff3e7e7d4ab18a7acf068235e0b2a147
hamzanaveedmalik <hamzanaveedmalik@gmail.com>
2018-02-22 19:22:30 +0500

    Pushing commits

--- src/components/PickEngineers.js
+++ src/components/PickEngineers.js
@@ -4,9 +4,8 @@ const PickEngineers = ({ selectTodaysEngineers }) => {
   return (
     <div
     onClick={() => selectTodaysEngineers()}
-    className=" f4 pv3 pa3 bg-black-30 tc pointer br0 ttu b">
-      Pick Engineers
-    </div>
+    className=" justify-center f4 pv3 pa3 bg-black-30 tc pointer br0 ttu b">
+Spin Wheel    </div>
   )
 };
 
